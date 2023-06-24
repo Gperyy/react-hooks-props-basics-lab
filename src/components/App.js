2b8cbb89e0b83f806b9bbc04abced96b10b251b1
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-06-24 14:16:27 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home name={user.name} city={user.city} color={user.color} />
+            <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
+        </div>
+    );
 }
 
 export default App;
